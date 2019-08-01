13d19dffdfdb1635c3f16b2c85b61ba8e9a3eeda
adamd1008 <a-dodd@outlook.com>
2019-08-02 00:28:51 +0100

    Many improvements

--- backup.py
+++ backup.py
@@ -23,6 +23,7 @@
 # SOFTWARE.
 
 from datetime import datetime, date, timedelta
+from enum import IntEnum
 import hashlib
 import json
 import os
@@ -36,9 +37,21 @@ import time
 sqlite3_create_tables = """
     DROP TABLE IF EXISTS `meta`;
     CREATE TABLE IF NOT EXISTS `meta` (
-        `name`                         TEXT NOT NULL,
-        `out_dir`                      TEXT NOT NULL,
-        `hash_excluded_files_max_size` INTEGER NOT NULL
+        `name`                          TEXT NOT NULL,
+        `out_dir`                       TEXT NOT NULL,
+        `hash_excluded_files_max_size`  INTEGER NOT NULL,
+        `start_time`                    INTEGER NOT NULL,
+        `end_time`                      INTEGER NOT NULL,
+        `files_allowed`                 INTEGER NOT NULL,
+        `files_allowed_bytes`           INTEGER NOT NULL,
+        `files_excluded`                INTEGER NOT NULL,
+        `files_excluded_bytes`          INTEGER NOT NULL,
+        `files_errored`                 INTEGER NOT NULL,
+        `files_hashed`                  INTEGER NOT NULL,
+        `sql_file_inserts`              INTEGER NOT NULL,
+        `sql_file_insert_time`          REAL NOT NULL,
+        `tar_add_time`                  REAL NOT NULL,
+        `hash_time`                     REAL NOT NULL
     );
     DROP TABLE IF EXISTS `in_dir`;
     CREATE TABLE IF NOT EXISTS `in_dir` (
@@ -85,14 +98,18 @@ sqlite3_insert_in_dir = """
 
 sqlite3_insert_meta = """
     INSERT INTO `meta` (
-        `name`, `out_dir`, `hash_excluded_files_max_size`
-        ) VALUES (?, ?, ?)
+        `name`, `out_dir`, `hash_excluded_files_max_size`,
+        `start_time`, `end_time`, `files_allowed`, `files_allowed_bytes`,
+        `files_excluded`, `files_excluded_bytes`, `files_errored`,
+        `files_hashed`, `sql_file_inserts`, `sql_file_insert_time`,
+        `tar_add_time`, `hash_time`
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 """
 
 sqlite3_insert_file = """
     INSERT INTO `file` (
         `path`, `size`, `date_accessed`, `date_modified`, `hash`, `error_code`
-        ) VALUES (?, ?, ?, ?, ?, ?)
+    ) VALUES (?, ?, ?, ?, ?, ?)
 """
 
 sqlite3_insert_error_code = """
@@ -108,12 +125,19 @@ sqlite3_insert_excluded_ext = """
 """
 
 
-error_codes = [
-    "Success",
-    "Excluded extension",
-    "IOError",
-    "TarError"
-]
+class ErrorCodes(IntEnum):
+    SUCCESS = 0
+    EXCL_EXT = 1
+    IO_ERROR = 2
+    TAR_ERROR = 3
+
+
+error_codes = {
+    ErrorCodes.SUCCESS:     "Success",
+    ErrorCodes.EXCL_EXT:    "Excluded extension",
+    ErrorCodes.IO_ERROR:    "IOError",
+    ErrorCodes.TAR_ERROR:   "TarError"
+}
 
 
 def hash_file(file_path):
@@ -126,13 +150,17 @@ def hash_file(file_path):
    return h.hexdigest()
 
 
+def datetime_to_unix(dt):
+    return int(time.mktime(dt.timetuple()))
+
+
 class DbAgent:
     def __init__(self, backup_name, out_dir, start_time):
         self.backup_name = backup_name
         self.out_dir = out_dir
         self.start_time = start_time
 
-        sqlite3.register_adapter(datetime, DbAgent.dt_adapter)
+        sqlite3.register_adapter(datetime, datetime_to_unix)
 
         self.db_name = self.backup_name + "-" + self.start_time + ".sqlite3"
         self.full_path = os.path.join(self.out_dir, self.db_name)
@@ -143,11 +171,6 @@ class DbAgent:
 
         self.cur = self.conn.cursor()
         self.cur.executescript(sqlite3_create_tables)
-
-
-    @staticmethod
-    def dt_adapter(dt):
-        return int(time.mktime(dt.timetuple()))
 
 
     #def __del__(self):
@@ -181,11 +204,15 @@ def validate_dirs(dirs):
 
 def insert_error_codes(db):
     cur = db.get_cursor()
-    data = [(i, v) for i, v in enumerate(error_codes)]
+    data = [(int(key), error_codes[key]) for key in error_codes]
     cur.executemany(sqlite3_insert_error_code, data)
 
 
-def insert_meta(db, backup_name, in_dirs, out_dir, excluded_exts, hefms):
+def insert_meta(
+        db, backup_name, in_dirs, out_dir, excluded_exts, hefms,
+        start_time, end_time, files_allowed, files_allowed_bytes,
+        files_excluded, files_excluded_bytes, files_errored, files_hashed,
+        sql_file_inserts, sql_file_insert_time, tar_add_time, hash_time):
     cur = db.get_cursor()
 
     in_dirs_tupled = [(in_dir,) for in_dir in in_dirs]
@@ -194,7 +221,12 @@ def insert_meta(db, backup_name, in_dirs, out_dir, excluded_exts, hefms):
     excluded_exts_tupled = [(ext,) for ext in excluded_exts]
     cur.executemany(sqlite3_insert_excluded_ext, excluded_exts_tupled)
 
-    cur.execute(sqlite3_insert_meta, (backup_name, out_dir, hefms))
+    cur.execute(
+            sqlite3_insert_meta,
+            (backup_name, out_dir, hefms, start_time, end_time, files_allowed,
+                files_allowed_bytes, files_excluded, files_excluded_bytes,
+                files_errored, files_hashed, sql_file_inserts,
+                sql_file_insert_time, tar_add_time, hash_time))
 
 
 def create_tar(backup_name, out_dir, start_time):
@@ -207,20 +239,24 @@ def create_tar(backup_name, out_dir, start_time):
     return tar
 
 
-def do_backup(db, tar, in_dirs, excluded_exts, hefms):
-    total_allowed = 0
-    total_allowed_bytes = 0
-    total_excluded = 0
-    total_excluded_bytes = 0
-    total_error = 0
-    total_file_inserts = 0
-    total_file_insert_time = 0.0
+def do_backup(db, tar, backup_name, in_dirs, out_dir, excluded_exts, hefms):
+    files_allowed = 0
+    files_allowed_bytes = 0
+    files_excluded = 0
+    files_excluded_bytes = 0
+    files_errored = 0
+    files_hashed = 0
+    sql_file_inserts = 0
+    sql_file_insert_time = 0.0
+    tar_add_time = 0.0
+    hash_time = 0.0
+
     cur = db.get_cursor()
 
     for in_dir in in_dirs:
         for root, dirs, files in os.walk(in_dir):
             for name in files:
-                file_status = 0
+                file_status = ErrorCodes.SUCCESS
                 file_stat = None
                 can_hash_file = False
                 hash_code = None
@@ -228,44 +264,57 @@ def do_backup(db, tar, in_dirs, excluded_exts, hefms):
                 file_ext = os.path.splitext(name)[1][1:]
 
                 if file_ext in excluded_exts:
-                    file_status = 1
+                    file_status = ErrorCodes.EXCL_EXT
 
                 try:
                     file_stat = os.stat(full_path)
 
-                    if (file_status == 0) or (file_stat.st_size <= hefms):
+                    if (file_status == ErrorCodes.SUCCESS) or \
+                        (file_stat.st_size <= hefms):
                         can_hash_file = True
                 except IOError as e:
-                    file_status = 2
+                    file_status = ErrorCodes.IO_ERROR
                     print(("IOError\n" +
-                        "> file     : '{}'\n" +
-                        "> errno    : {}\n" +
-                        "> args     : {}\n" +
-                        "> message  : {}\n" +
-                        "> strerror : {}").format(
-                            full_path, e.errno, e.args, e.message, e.strerror))
+                            "> file     : '{}'\n" +
+                            "> errno    : {}\n" +
+                            "> args     : {}\n" +
+                            "> message  : {}\n" +
+                            "> strerror : {}").format(
+                                full_path, e.errno, e.args,
+                                e.message, e.strerror))
 
                 if can_hash_file:
+                    perf_counter_before = time.perf_counter()
                     hash_code = hash_file(full_path)
+                    perf_counter_after = time.perf_counter()
 
-                if file_status == 0:
+                    files_hashed += 1
+                    hash_time += \
+                            perf_counter_after - perf_counter_before
+
+                if file_status == ErrorCodes.SUCCESS:
                     try:
+                        perf_counter_before = time.perf_counter()
                         tar.add(full_path)
-                        total_allowed += 1
-                        total_allowed_bytes += file_stat.st_size
-                    except tarfile.TarError as e:
-                        file_status = 3
-                        print(("TarError\n" +
-                            "> file     : '{}'\n" +
-                            "> args     : {}\n" +
-                            "> message  : {}").format(
-                                full_path, e.args, e.message))
+                        perf_counter_after = time.perf_counter()
 
-                if file_status == 1:
-                    total_excluded += 1
-                    total_excluded_bytes += file_stat.st_size
-                elif file_status != 0:
-                    total_error += 1
+                        files_allowed += 1
+                        files_allowed_bytes += file_stat.st_size
+                        tar_add_time += \
+                                perf_counter_after - perf_counter_before
+                    except tarfile.TarError as e:
+                        file_status = ErrorCodes.TAR_ERROR
+                        print(("TarError\n" +
+                                "> file     : '{}'\n" +
+                                "> args     : {}\n" +
+                                "> message  : {}").format(
+                                    full_path, e.args, e.message))
+
+                if file_status == ErrorCodes.EXCL_EXT:
+                    files_excluded += 1
+                    files_excluded_bytes += file_stat.st_size
+                elif file_status != ErrorCodes.SUCCESS:
+                    files_errored += 1
 
                 if file_stat != None:
                     size = file_stat.st_size
@@ -279,42 +328,33 @@ def do_backup(db, tar, in_dirs, excluded_exts, hefms):
                 try:
                     perf_counter_before = time.perf_counter()
                     cur.execute(
-                        sqlite3_insert_file,
-                        (full_path, size, date_accessed, date_modified,
-                            hash_code, file_status))
+                            sqlite3_insert_file,
+                            (full_path, size, date_accessed, date_modified,
+                                hash_code, file_status))
                     perf_counter_after = time.perf_counter()
 
-                    total_file_inserts += 1
-                    total_file_insert_time += \
-                        perf_counter_after - perf_counter_before
+                    sql_file_inserts += 1
+                    sql_file_insert_time += \
+                            perf_counter_after - perf_counter_before
 
 
                 except sqlite3.IntegrityError as e:
                     print(("IntegrityError\n" +
-                        "> file      : '{}'\n" +
-                        "> args      : {}").format(
-                            full_path, e.args))
+                            "> file      : '{}'\n" +
+                            "> args      : {}").format(
+                                full_path, e.args))
 
-    print("Allowed files       : {}".format(total_allowed))
-    print("Allowed files size  : {} ({} MB)".format(
-        total_allowed_bytes, total_allowed_bytes / 1000000))
-    print("Excluded files      : {}".format(total_excluded))
-    print("Excluded files size : {} ({} MB)".format(
-        total_excluded_bytes, total_excluded_bytes / 1000000))
-    print("Errored files       : {}".format(total_error))
-    print("File inserts        : {}".format(total_file_inserts))
-    print("File insert time    : {}".format(total_file_insert_time))
-
-    if total_file_inserts > 0:
-        print("Avg time per file insert : {}".format(
-            total_file_insert_time / float(total_file_inserts)))
+    return files_allowed, files_allowed_bytes, files_excluded, \
+            files_excluded_bytes, files_errored, files_hashed, \
+            sql_file_inserts, sql_file_insert_time, tar_add_time, hash_time
 
 
 def main(config):
-    time_now = datetime.now().strftime("%y%m%d-%H%M%S")
+    start_time = datetime.now()
+    start_time_str = start_time.strftime("%y%m%d-%H%M%S")
+    print("Started at  {}".format(start_time_str))
 
     j = json.loads(open(config).read())
-    print("j = {}".format(j))
 
     backup_name = j["name"]
     in_dirs = j["inDirs"]
@@ -325,17 +365,56 @@ def main(config):
     validate_dirs(in_dirs)
     validate_dir(out_dir, os.R_OK | os.W_OK | os.X_OK)
 
-    tar = create_tar(backup_name, out_dir, time_now)
-    db = DbAgent(backup_name, out_dir, time_now)
+    tar = create_tar(backup_name, out_dir, start_time_str)
+    db = DbAgent(backup_name, out_dir, start_time_str)
 
     insert_error_codes(db)
-    insert_meta(
-            db, backup_name, in_dirs, out_dir, excluded_exts,
-            hash_excluded_files_max_size)
 
-    do_backup(db, tar, in_dirs, excluded_exts, hash_excluded_files_max_size)
+    files_allowed, files_allowed_bytes, files_excluded, \
+        files_excluded_bytes, files_errored, files_hashed, \
+        sql_file_inserts, sql_file_insert_time, tar_add_time, \
+        hash_time = do_backup(
+                        db, tar, backup_name, in_dirs, out_dir,
+                        excluded_exts, hash_excluded_files_max_size) 
 
     tar.close()
+
+    print("Allowed files            : {}".format(files_allowed))
+    print("Allowed files size       : {} ({} MB)".format(
+            files_allowed_bytes, files_allowed_bytes / 1000000))
+    print("Excluded files           : {}".format(files_excluded))
+    print("Excluded files size      : {} ({} MB)".format(
+            files_excluded_bytes, files_excluded_bytes / 1000000))
+    print("Errored files            : {}".format(files_errored))
+    print("Hashed files             : {}".format(files_hashed))
+    print("SQL `file` inserts       : {}".format(sql_file_inserts))
+    print("SQL `file insert time    : {}".format(sql_file_insert_time))
+    print("Tar add time             : {}".format(tar_add_time))
+    print("Hash time                : {}".format(hash_time))
+
+    if sql_file_inserts > 0:
+        print("Avg time per file insert : {}".format(
+                sql_file_insert_time / float(sql_file_inserts)))
+
+    if files_allowed > 0:
+        print("Avg time per tar add     : {}".format(
+                tar_add_time / float(files_allowed)))
+
+    if files_hashed > 0:
+        print("Avg time per hash        : {}".format(
+                hash_time / float(files_hashed)))
+
+    end_time = datetime.now()
+
+    print("Finished at {}".format(end_time.strftime("%y%m%d-%H%M%S")))
+
+    insert_meta(
+            db, backup_name, in_dirs, out_dir, excluded_exts,
+            hash_excluded_files_max_size, datetime_to_unix(start_time),
+            datetime_to_unix(end_time), files_allowed, files_allowed_bytes,
+            files_excluded, files_excluded_bytes, files_errored, files_hashed,
+            sql_file_inserts, sql_file_insert_time, tar_add_time, hash_time)
+
     db.close()
 
 
